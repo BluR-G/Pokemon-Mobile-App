c886df6936ab16e920f5d9434965ac5b288ed699
Dan Willis <dannjoumene@gmail.com>
2022-11-12 19:43:30 -0500

    Create a PokemonCollection class

---
+++ app/src/main/java/com/example/pokemon/objects/PokemonCollection.kt
@@ -0,0 +1,27 @@
+package com.example.pokemon.objects
+
+import android.widget.Toast
+
+class PokemonCollection {
+    private lateinit var pokemons: ArrayList<Pokemon>
+
+    fun addPokemonToCollection(pokemon: Pokemon){
+            this.pokemons.add(pokemon)
+    }
+
+    fun removePokemonFromCollection(pokemon: Pokemon){
+        this.pokemons.remove(pokemon)
+    }
+
+
+    fun switchPokemonToCollection(pokemon: Pokemon, pokemonTeam: PokemonTeam) {
+        pokemonTeam.removePokemonFromTeam(pokemon)
+        addPokemonToCollection(pokemon)
+    }
+
+    fun switchPokemonTeamToCollection(pokemonInTeam: Pokemon, pokemonTeam: PokemonTeam, pokemonInCollection: Pokemon){
+        pokemonTeam.removePokemonFromTeam(pokemonInTeam)
+        addPokemonToCollection(pokemonInTeam)
+        pokemonTeam.addPokemonToTeam(pokemonInCollection)
+    }
+}
--- app/src/main/java/com/example/pokemon/objects/PokemonTeam.kt
+++ app/src/main/java/com/example/pokemon/objects/PokemonTeam.kt
@@ -3,9 +3,23 @@ package com.example.pokemon.objects
 class PokemonTeam {
     private lateinit var pokemons: ArrayList<Pokemon>
 
-    fun addPokemon(pokemon: Pokemon){
+    fun addPokemonToTeam(pokemon: Pokemon): Boolean{
         if(pokemons.size < 6){
             this.pokemons.add(pokemon)
+            return true
+        }
+        return false
+    }
+
+    fun removePokemonFromTeam(pokemon: Pokemon){
+        this.pokemons.remove(pokemon)
+    }
+
+    fun switchPokemonToTeam(pokemon: Pokemon, pokemonCollection: PokemonCollection){
+        pokemonCollection.removePokemonFromCollection(pokemon)
+        val isSuccess = addPokemonToTeam(pokemon)
+        if (!isSuccess){
+            //Toast.makeText(this@MainMenuActivity, "Pokemon Team is full", Toast.LENGTH_SHORT).show()
         }
     }
 
