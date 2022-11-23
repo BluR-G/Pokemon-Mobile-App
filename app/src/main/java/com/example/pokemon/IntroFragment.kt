5d435fc4ab6288412acbddfa9b8bdea50b962864
Ray Hernaez <rayhernz@outlook.com>
2022-11-22 20:12:33 -0500

    Disable button if username input is blank/space

--- app/src/main/java/com/example/pokemon/IntroFragment.kt
+++ app/src/main/java/com/example/pokemon/IntroFragment.kt
@@ -2,6 +2,8 @@ package com.example.pokemon
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,6 +23,19 @@ class IntroFragment : Fragment() {
             val intent = Intent(activity, MenuActivity::class.java)
             startActivity(intent)
         }
+
+        binding.usernameInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString().trim() == "") {
+                    binding.IntroGoToMainMenu.isEnabled = false;
+                }
+                else {
+                    binding.IntroGoToMainMenu.isEnabled = true;
+                }
+            }
+        })
 
         return binding.root
     }
