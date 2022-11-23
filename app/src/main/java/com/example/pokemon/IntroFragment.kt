9a45a49559e091fa8a2072bdd4d7fddca2ff85df
Ray Hernaez <rayhernz@outlook.com>
2022-11-22 21:24:23 -0500

    Enable button only when name is provided and a starter is picked

--- app/src/main/java/com/example/pokemon/IntroFragment.kt
+++ app/src/main/java/com/example/pokemon/IntroFragment.kt
@@ -19,26 +19,49 @@ class IntroFragment : Fragment() {
     ): View? {
         val binding = FragmentIntroBinding.inflate(layoutInflater)
 
+        var isUserNameValid : Boolean = false;
+        var isStarterPicked : Boolean = false;
+
         binding.IntroGoToMainMenu.setOnClickListener {
             val intent = Intent(activity, MenuActivity::class.java)
             startActivity(intent)
         }
 
+        // Enable button if name is provided and a radiobutton is checked
         binding.usernameInput.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                if (s.toString().trim() == "") {
-                    binding.IntroGoToMainMenu.isEnabled = false;
-                }
-                else {
-                    binding.IntroGoToMainMenu.isEnabled = true;
+                if (s.toString().trim() != "") {
+                    isUserNameValid = true;
+                    if (isStarterPicked) {
+                        binding.IntroGoToMainMenu.isEnabled = true;
+                    }
                 }
             }
         })
 
+        // Enable button if a radiobutton is checked and name is provided
+        binding.bulbasaurRBtn.setOnClickListener {
+            isStarterPicked = true;
+            if (isUserNameValid) {
+                binding.IntroGoToMainMenu.isEnabled = true;
+            }
+        }
+        binding.charmanderRBtn.setOnClickListener {
+            isStarterPicked = true;
+            if (isUserNameValid) {
+                binding.IntroGoToMainMenu.isEnabled = true;
+            }
+        }
+        binding.squirtleRBtn.setOnClickListener {
+            isStarterPicked = true;
+            if (isUserNameValid) {
+                binding.IntroGoToMainMenu.isEnabled = true;
+            }
+        }
+
         return binding.root
     }
-
 
 }
