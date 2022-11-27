af04152dee875b5d2cae0ee9cd4639d1a359e595
Robert_G <shadow-of-darknes@live.ca>
2022-11-27 13:35:02 -0500

    disable pokemon team / collection initialization in MenuActivity

--- app/src/main/java/com/example/pokemon/menu/MenuActivity.kt
+++ app/src/main/java/com/example/pokemon/menu/MenuActivity.kt
@@ -17,110 +17,110 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        pokemonTeam = initializeTeam()
-        pokemonCollection = initializeCollection()
+        pokemonTeam = PokemonTeam()
+        pokemonCollection = PokemonCollection()
     }
-    private fun initializeCollection(): PokemonCollection {
-        val pokemonCollection = PokemonCollection()
-        val types = arrayOf<String>("fire")
-        val moves = ArrayList<MoveData>()
-        addMoves(moves)
-        val pikachuMoves = ArrayList<MoveData>()
-        addPikachuMoves(pikachuMoves)
-        val images = arrayOf<Drawable>()
-
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("charizard","charizard",36,types,
-                200,30,40,100,200,100, moves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("pikachu","pikachu",36,types,
-                60,30,40,100,200,100, pikachuMoves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("raichu","raichu",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("charizard","charizard",36,types,
-                200,30,40,100,200,100, moves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("pikachu","pikachu",36,types,
-                60,30,40,100,200,100, pikachuMoves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("raichu","raichu",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("charizard","charizard",36,types,
-                200,30,40,100,200,100, moves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("pikachu","pikachu",36,types,
-                60,30,40,100,200,100, pikachuMoves, images)
-        )
-        pokemonCollection.addPokemonToCollection(
-            Pokemon("raichu","raichu",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-
-        return pokemonCollection
-    }
-
-    private fun initializeTeam(): PokemonTeam {
-        val pokemonTeam = PokemonTeam()
-        val types = arrayOf<String>("fire")
-        val moves = ArrayList<MoveData>()
-        addMoves(moves)
-        val pikachuMoves = ArrayList<MoveData>()
-        addPikachuMoves(pikachuMoves)
-        val images = arrayOf<Drawable>()
-
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("charizard","charizard",36,types,
-                200,30,40,100,200,100, moves, images)
-        )
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("pikachu","pikachu",36,types,
-                60,30,40,100,200,100, pikachuMoves, images)
-        )
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("raichu","raichu",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("garyados","gyarados",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("raichu","zekrom",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-        pokemonTeam.addPokemonToTeam(
-            Pokemon("garyados","reshiram",36,types,
-                60,30,40,100,200,100, moves, images)
-        )
-
-        return pokemonTeam
-    }
-    private fun addMoves(moves: ArrayList<MoveData>){
-        val types = arrayOf("fire")
-        val move1 = Move(100,40,"other","special", "", 40, types)
-        moves.add(MoveData("ember", 5,move1))
-        moves.add(MoveData("whip", 5,move1))
-        moves.add(MoveData("tackle", 5,move1))
-        moves.add(MoveData("sleep", 5,move1))
-    }
-    private fun addPikachuMoves(moves: ArrayList<MoveData>){
-        val types= arrayOf("electric")
-        val move = Move(100,40,"other","special", "", 40, types)
-        moves.add(MoveData("Thunderbolt", 5,move))
-        moves.add(MoveData("Swift tackle", 5,move))
-        moves.add(MoveData("Roll", 5,move))
-        moves.add(MoveData("Static", 5,move))
-    }
+//    private fun initializeCollection(): PokemonCollection {
+//        val pokemonCollection = PokemonCollection()
+//        val types = arrayOf<String>("fire")
+//        val moves = ArrayList<MoveData>()
+//        addMoves(moves)
+//        val pikachuMoves = ArrayList<MoveData>()
+//        addPikachuMoves(pikachuMoves)
+//        val images = arrayOf<Drawable>()
+//
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("charizard","charizard",36,types,
+//                200,30,40,100,200,100, moves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("pikachu","pikachu",36,types,
+//                60,30,40,100,200,100, pikachuMoves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("raichu","raichu",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("charizard","charizard",36,types,
+//                200,30,40,100,200,100, moves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("pikachu","pikachu",36,types,
+//                60,30,40,100,200,100, pikachuMoves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("raichu","raichu",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("charizard","charizard",36,types,
+//                200,30,40,100,200,100, moves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("pikachu","pikachu",36,types,
+//                60,30,40,100,200,100, pikachuMoves, images)
+//        )
+//        pokemonCollection.addPokemonToCollection(
+//            Pokemon("raichu","raichu",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//
+//        return pokemonCollection
+//    }
+//
+//    private fun initializeTeam(): PokemonTeam {
+//        val pokemonTeam = PokemonTeam()
+//        val types = arrayOf<String>("fire")
+//        val moves = ArrayList<MoveData>()
+//        addMoves(moves)
+//        val pikachuMoves = ArrayList<MoveData>()
+//        addPikachuMoves(pikachuMoves)
+//        val images = arrayOf<Drawable>()
+//
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("charizard","charizard",36,types,
+//                200,30,40,100,200,100, moves, images)
+//        )
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("pikachu","pikachu",36,types,
+//                60,30,40,100,200,100, pikachuMoves, images)
+//        )
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("raichu","raichu",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("garyados","gyarados",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("raichu","zekrom",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//        pokemonTeam.addPokemonToTeam(
+//            Pokemon("garyados","reshiram",36,types,
+//                60,30,40,100,200,100, moves, images)
+//        )
+//
+//        return pokemonTeam
+//    }
+//    private fun addMoves(moves: ArrayList<MoveData>){
+//        val types = arrayOf("fire")
+//        val move1 = Move(100,40,"other","special", "", 40, types)
+//        moves.add(MoveData("ember", 5,move1))
+//        moves.add(MoveData("whip", 5,move1))
+//        moves.add(MoveData("tackle", 5,move1))
+//        moves.add(MoveData("sleep", 5,move1))
+//    }
+//    private fun addPikachuMoves(moves: ArrayList<MoveData>){
+//        val types= arrayOf("electric")
+//        val move = Move(100,40,"other","special", "", 40, types)
+//        moves.add(MoveData("Thunderbolt", 5,move))
+//        moves.add(MoveData("Swift tackle", 5,move))
+//        moves.add(MoveData("Roll", 5,move))
+//        moves.add(MoveData("Static", 5,move))
+//    }
 
     public fun getBind(): ActivityMenuBinding {
         return this.binding
