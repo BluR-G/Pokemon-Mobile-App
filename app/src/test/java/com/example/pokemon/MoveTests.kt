7a31cd5a3dd873a0ec37ae1cc46ddaaed0238945
Dan Willis <dannjoumene@gmail.com>
2022-12-04 14:15:29 -0500

    Add test for Move class

---
+++ app/src/test/java/com/example/pokemon/MoveTests.kt
@@ -0,0 +1,21 @@
+package com.example.pokemon
+
+import com.example.pokemon.objects.Move
+import org.junit.Assert
+import org.junit.Test
+
+class MoveTests {
+
+    @Test
+    fun createMove() {
+        val move = Move(100, 40, "PHYSICAL", 0, "OPPONENT", "NONE", 0, "normal")
+        Assert.assertEquals(100, move.getAccuracy())
+        Assert.assertEquals(40, move.getPower())
+        Assert.assertEquals("PHYSICAL", move.getDamageClass())
+        Assert.assertEquals(0, move.getHeal())
+        Assert.assertEquals("OPPONENT", move.getTarget())
+        Assert.assertEquals("NONE", move.getEffect())
+        Assert.assertEquals("normal", move.getTypes())
+        Assert.assertEquals(0, move.getEffectChance())
+    }
+}
