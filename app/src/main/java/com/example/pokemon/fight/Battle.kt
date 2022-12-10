4903bcf380f85ad943621d387c587b44bdc04a56
johnnyhoaang <johnny.hoang@dawsoncollege.qc.ca>
2022-12-10 13:48:22 -0500

    Remove XP Boost

--- app/src/main/java/com/example/pokemon/fight/Battle.kt
+++ app/src/main/java/com/example/pokemon/fight/Battle.kt
@@ -410,7 +410,7 @@ abstract class Battle {
     // Add experience to pokemon when fight is won
     public fun addExperience(){
         val expGain = 0.3 * getCurrentEnemyPokemon().getExperienceReward() * getCurrentEnemyPokemon().getLevel()
-        currentAllyPokemon.addExperience(expGain.toInt()+2000)
+        currentAllyPokemon.addExperience(expGain.toInt())
     }
 }
 
